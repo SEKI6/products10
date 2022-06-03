35bd4d7d4fb28b9300a17d33067e77f68b8908fc
SEKI6 <andjal67@gmail.com>
2022-06-03 11:50:26 +0200

    first commit

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = process.env.PORT||3000
+var bodyParser = require('body-parser')
+
+const port = process.env.PORT||3002
 app.use(cors())
 
 app.use(express.static('public'))
 
-const imgUrl='https://kulhavikruno.000webhostapp.com/diois_img/'
-const items=[
-{name:'iPhone11', description:'descr',price: 4800, category:'mobile', brand:'Apple',
-url:imgUrl+'iPhone11.jpg'},
-{name:'iPhone11', description:'descr',price: 6200, category:'mobile', brand:'Apple',
-url:imgUrl+'iPhone12.jpg'},
-{name:'iPhone11', description:'descr',price: 7800, category:'mobile', brand:'Apple',
-url:imgUrl+'iPhone13.jpg'},
-{name:'A70', description:'descr',price: 400, category:'mobile', brand:'Samsung',
-url:imgUrl+'a70.jpg'},
-{name:'A71', description:'descr',price: 500, category:'mobile', brand:'Samsung',
-url: imgUrl+'a71.jpg'},
-{name:'S21', description:'descr',price: 700, category:'mobile', brand:'Samsung',
-url:imgUrl+'s21.jpg'}
+// parse application/json
+app.use(bodyParser.json())
+
+const players=[
+    {name:'Miki', score:10},
+    {name:'Aco', score:12},
+    {name:'Pero', score:3}
 ]
 app.get('/api', (req, res) => {
 //res.send(items.sort((a,b)=>a.price-b.price))
 //res.send(items.filter(a=>a.price>500))
 //res.send(items.filter(a=>a.brand==='Samsung'))
 //res.send(items.filter(a=>a.category!='mobile'))
-res.send(items)
+res.send(players)
 })
-app.get('/api/total',(req,res)=>{
-const total=items.reduce((acc,value)=>acc+value.price,0)
-res.send(total.toString())
+
+app.post('/api', (req, res)=>{
+    //console.log()
+    players.push(req.body)
+    res.send(players.sort((a,b)=>b.score-a.score))
 })
+
 app.listen(port)
--- public/index.js
+++
@@ -1,6 +0,0 @@
-
-
-//fetch('https://products-diois.herokuapp.com/')
-fetch('/api')
-.then(data=>data.json())
-.then(data=>console.log(data))
